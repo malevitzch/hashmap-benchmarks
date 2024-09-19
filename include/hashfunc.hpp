a338ddb5620a36cee2f7aeabd3d23165832fef71
malevitzch <malevitzch@gmail.com>
2024-09-19 18:17:20 +0200

    Created a baseline for a polynomial hash function

---
+++ include/hashfunc.hpp
@@ -0,0 +1,2 @@
+#include <string>
+unsigned int polynomial_hash(unsigned int base, unsigned int modulo, std::string input);
--- src/hashfunc.cpp
+++ src/hashfunc.cpp
@@ -1,1 +1,16 @@
 #include "hashfunc.hpp"
+#
+unsigned int polynomial_hash(unsigned int base, unsigned int modulo, std::string input)
+{
+  unsigned int hash = 0;
+  for(char ch : input)
+  {
+    hash += ch;
+    hash *= base;
+    hash %= modulo;
+    //there might be overflows but for unsigned integers in c++ this is intended behavior
+    //although when translating that might be a problem sometimes
+  }
+  return hash;
+}
+
