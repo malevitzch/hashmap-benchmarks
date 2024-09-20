2a7276c9a103f33b5e269937aab83df55992e756
malevitzch <malevitzch@gmail.com>
2024-09-20 13:43:33 +0200

    Finished implementing

--- src/hashmap.cpp
+++ src/hashmap.cpp
@@ -10,6 +10,15 @@ void Hashmap::update_mem()
   //if there is no need to resize then we do nothing
   if(!need_resize()) return;
   //TODO: implement resize logic
+  auto contents = get_all();
+  //reset mem, increasing the size twofold
+  mem = (typeof(mem))(2*mem.size());
+  for(auto&[key, val] : contents)
+  {
+    //this might look sus but it's just me being lazy and calling the operator rather than doing this manually
+    //obviously in the final code we want as little abstraction as possible, probably best to then not reuse functions (unless binary size is what matters more than stack usage)
+    (*this)[key] = val;
+  }
 }
 //NOTE: recommended initial_mem_size is 16, definitely more for the project, load_factor probably around 0.9-1 maybe higher but this needs tests (performance degrades quite fast with higher load factors)
 Hashmap::Hashmap(unsigned int initial_mem_size, float load_factor, typeof(hash_function) hash_function)
