b16fafe53e0389cf90601c82a1e78afd0d0e22b9
malevitzch <malevitzch@gmail.com>
2024-09-20 13:47:04 +0200

    Added initial tests

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include <functional>
 #include "hashfunc.hpp"
+#include "hashmap.hpp"
 
 int main()
 {
-  auto example_poly = std::bind(polynomial_hash, 743, 104729, std::placeholders::_1);
-  std::string hello = "Hello Hashmap";
-  std::cout<<example_poly(hello);
+  auto example_poly = std::bind(polynomial_hash, 743, std::placeholders::_2, std::placeholders::_1);
+  Hashmap map(16, 0.9, example_poly);
+  map["test"] = 3;
+  std::cout<<map["test"];
 }
