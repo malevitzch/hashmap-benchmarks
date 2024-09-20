398428a905f31d48f0a5ad814519182efedee43a
malevitzch <malevitzch@gmail.com>
2024-09-20 11:25:27 +0200

    Implemented need_resize function and get_all, fixed implementation for operator[] to properly increment the number of elements in hashmap in case an element is added

--- src/hashmap.cpp
+++ src/hashmap.cpp
@@ -1,5 +1,16 @@
 #include "hashmap.hpp"
 
+bool Hashmap::need_resize()
+{
+  return load_factor * mem.size() < elements;
+}
+
+void Hashmap::update_mem()
+{
+  //if there is no need to resize then we do nothing
+  if(!need_resize()) return;
+  //TODO: implement resize logic
+}
 //NOTE: recommended initial_mem_size is 16, definitely more for the project, load_factor probably around 0.9-1 maybe higher but this needs tests (performance degrades quite fast with higher load factors)
 Hashmap::Hashmap(unsigned int initial_mem_size, float load_factor, typeof(hash_function) hash_function)
   : mem(initial_mem_size), hash_function(hash_function), load_factor(load_factor) {}
@@ -13,6 +24,8 @@ int& Hashmap::operator[](std::string key)
   }
   //add a dummy element in case there is no element with that key
   mem[hash].push_back({key, 0});
+  //increment the elements since this is a new element we are adding
+  elements++;
   //return a reference to the new element so that you can do: map[x] = 10;
   return mem[hash].back().second;
 }
@@ -24,6 +37,11 @@ bool Hashmap::contains(std::string key)
 
 std::vector<std::pair<std::string, int> > Hashmap::get_all()
 {
-
+  std::vector<std::pair<std::string, int> > res;
+  for(auto& vec : mem)
+  {
+    for(auto& el : vec) res.push_back(el);
+  }
+  return res;
 }
 
